3b97acb185666cdf474ec28a0508e7aff6d240fb
Petter Fors <PANDER@ad01.migrationsverket.se>
2017-05-24 20:42:14 +0200

    First Commit

---
+++ Public/Get-APICEMTicket.ps1
@@ -0,0 +1,41 @@
+<#
+
+.SYNOPSIS
+ GET serviceTicket from APIC Controller
+
+.DESCRIPTION
+POST request to get API Key/ServiceTicket
+
+.EXAMPLE
+$APICticket = Get-APICEMTicket -Credential $APICcred -Computername $APICEM
+
+#>
+function Get-APICEMTicket{
+    Param(
+    [Parameter(Mandatory = $true,Position = 0,HelpMessage = 'Credentials')]
+    [ValidateNotNullorEmpty()]
+    [System.Management.Automation.PSCredential]$Credential,
+    [Parameter(Mandatory = $true,Position = 1,HelpMessage = "hostname.domain.com")]
+    [String]$Computername
+
+    )
+    Begin{
+ $URIaddress = "https://$Computername/api/v1/ticket"
+
+ $UserName = $Credential.GetNetworkCredential().UserName
+ $Password = $Credential.GetNetworkCredential().Password
+ $JsonBody= @"
+{
+    "username" : "$UserName",
+    "password" : "$Password"
+}
+"@
+}
+    Process {
+   $JsonBody
+    $response = Invoke-WebRequest -Uri $URIaddress -ContentType 'application/json'-Method Post -Body $JsonBody
+    $response = $response.content | ConvertFrom-Json
+    return $response
+#tray
+}
+}
